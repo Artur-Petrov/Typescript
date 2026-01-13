a62049c5db85b0a3aebf8e40e24cd6430f927a96
Artur-Petrov <petrov.artur02@gmail.com>
2026-01-13 22:16:49 +0200

    lesson 2 - bases types

---
+++ Classwork/Lesson2/main.ts
@@ -0,0 +1,10 @@
+let aa: number = 123;
+let bb: boolean = true;
+let cc: string = 'abc';
+
+function foo(arg1: number, arg2: number):number {
+    return arg1 + arg2;
+}
+
+let arr: number[] = [];
+arr.push(1);
