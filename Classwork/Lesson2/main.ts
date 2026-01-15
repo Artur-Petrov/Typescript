ea4423c638d62a8ed7f30e3964a5f1ee3879c641
Artur-Petrov <petrov.artur02@gmail.com>
2026-01-15 20:25:54 +0200

    lesson 1 - Type

--- Classwork/Lesson2/main.ts
+++ Classwork/Lesson2/main.ts
@@ -1,10 +1,33 @@
-let aa: number = 123;
-let bb: boolean = true;
-let cc: string = 'abc';
+// let aa: number = 123;
+// let bb: boolean = true;
+// let cc: string = 'abc';
+//
+// function foo(arg1: number, arg2: number):number {
+//     return arg1 + arg2;
+// }
+//
+// let arr: number[] = [];
+// arr.push(1);
 
-function foo(arg1: number, arg2: number):number {
-    return arg1 + arg2;
-}
+// Type
 
-let arr: number[] = [];
-arr.push(1);
+type UserType = {
+    name: string,
+    age: number,
+    status: boolean
+    asd?: number // ? означає необов'язкове поле
+};
+
+let users: UserType[] = [
+    {name: 'vasya', age: 31, status: false, asd: 123},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
