69222e6385f7376d3eb09379cd469f136d3e93fe
Artur-Petrov <petrov.artur02@gmail.com>
2026-01-23 21:25:23 +0200

    lesson 2 - alias

---
+++ Classwork/Lesson2/alias/alias.ts
@@ -0,0 +1,8 @@
+type MyString = string;
+let s: MyString = 'wqqsqsqsq';
+
+type PrimitiveTypes = string | number | boolean;
+let x: PrimitiveTypes = 12132;
+
+type Falsy = 0 | '' | null| undefined | false ;
+let f: Falsy = 0;
